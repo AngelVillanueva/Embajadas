0fd7b5a77135141e8c7bbbcf86dbac43989821a9
Angel Villanueva <avillanueva@sinapse.es>
2012-11-23 16:17:05 +0100

    Initial setup, including test environment (rspec, cucumber, spork, guard)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Embajadas::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # user / pwd protected
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Embajadas") do |u, p|
+    [u, p] == ['embajadas', 'embajadas']
+  end
 end
