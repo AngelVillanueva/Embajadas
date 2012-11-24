d573e4e3bb977749d9fb2406d7089a0140ecf46b
Angel Villanueva <avillanueva@sinapse.es>
2012-11-24 20:55:31 +0100

    Embassy model: name accessible and unique

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -1,2 +1,5 @@
 class Embassy < ActiveRecord::Base
+  attr_accessible :name
+
+  validates :name, uniqueness: true
 end
--- spec/requests/embassies_spec.rb
+++ spec/requests/embassies_spec.rb
@@ -8,4 +8,17 @@ describe "Embassies" do
     it { should respond_to :name}
     it { should be_valid }
   end
+  describe "with accessible attributes" do
+    let(:embassy) { Embassy.new(name: "Brand name") }
+    subject { embassy }
+
+    it { should be_valid }
+  end
+  describe "with a unique name atribute" do
+    let(:embassy) { FactoryGirl.create(:embassy, name: "Brand name") }
+    let(:embassy_with_same_name) { embassy.dup }
+    subject { embassy_with_same_name }
+
+    it { should_not be_valid }
+  end
 end
