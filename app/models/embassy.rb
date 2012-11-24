a7e32bfe7c79f6d3149c9412f1827e04c3f65aa5
Angel Villanueva <avillanueva@sinapse.es>
2012-11-24 21:11:56 +0100

    Embassy annotated

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: embassies
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Embassy < ActiveRecord::Base
   attr_accessible :name
 
