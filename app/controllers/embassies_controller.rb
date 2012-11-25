98ae35cfb125ca5c3ca82ce024cc088c6141e57d
Angel Villanueva <avillanueva@sinapse.es>
2012-11-25 20:13:40 +0100

    Filter helper in Embassies Controller (not working yet)

--- app/controllers/embassies_controller.rb
+++ app/controllers/embassies_controller.rb
@@ -1,8 +1,18 @@
 class EmbassiesController < ApplicationController
+  # before_filter :require_belonging, only: [:show]
+  
   def welcome
   end
   def show
     @embassy = Embassy.find(params[:id])
     @missions = @embassy.missions
+  end
+
+  private
+  def require_belonging
+    unless belongs_to_embassy?
+      flash[:error] = I18n.t("flash.Access not allowed")
+      redirect_to root_path
+    end
   end
 end
