840d3fec131ac8488ab18626e0250aa852d39c70
Angel Villanueva <avillanueva@sinapse.es>
2012-12-03 17:00:53 +0100

    Authentication and Belonging requirement extend to Mission pages

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,10 @@
 class MissionsController < ApplicationController
+  before_filter :authenticate_ambassador!, only: [:show]
+  before_filter :require_belonging, only: [:show]
+  
   def show
     @mission = Mission.find(params[:id])
     @rewards = @mission.rewards
   end
+
 end
