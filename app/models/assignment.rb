7124592ee69fc9079aa9db3dfcdbb47f47d9dce3
Angel Villanueva <avillanueva@sinapse.es>
2013-01-30 16:30:18 +0100

    Assignment model annotated

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: assignments
+#
+#  id            :integer          not null, primary key
+#  code          :string(255)
+#  tracking_url  :text
+#  short_url     :string(255)
+#  mission_id    :integer
+#  ambassador_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Assignment < ActiveRecord::Base
   belongs_to :ambassador
   belongs_to :mission
