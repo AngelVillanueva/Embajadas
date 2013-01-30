46cceea7d9ef4b2929d497199f8d5170866e7757
Angel Villanueva <avillanueva@sinapse.es>
2013-01-30 15:32:44 +0100

    Cookie reader refactoring, using images as a flag for testing purposes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,18 +11,21 @@ class StaticPagesController < ApplicationController
 
   def cookie_reader
     @value = cookies[:ambassador]
+    image = 'gate.gif' # using the different images as a flag for testing purposes, in production 1x1 empty gif file
     if @value
       ambassador = Ambassador.where(tracking_id: cookies[:ambassador]).first
       mission = Mission.where(tracking_id: params[:tmis]).first
+      image = 'sandal.gif'
+      if ambassador && mission
+        @point = Point.new
+        @point.ambassador = ambassador
+        @point.mission = mission
 
-      @point = Point.new
-      @point.ambassador = ambassador
-      @point.mission = mission
-
-      @point.save!
-
-      send_file Rails.root.join("public", "bonsai.gif"), type: "image/gif", disposition: "inline"
+        @point.save!
+        image = 'fan.gif'
+      end
     end
+    send_file Rails.root.join("public", image), type: "image/gif", disposition: "inline"
   end
 
   def pixel_test
