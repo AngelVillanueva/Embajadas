a0d0e97770660a47eea21760eb619054095e7297
Angel Villanueva <avillanueva@sinapse.es>
2013-01-30 16:28:16 +0100

    Get rid of previous pixel feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -27,45 +27,4 @@ class StaticPagesController < ApplicationController
     end
     send_file Rails.root.join("public", image), type: "image/gif", disposition: "inline"
   end
-
-  def pixel_test
-    seed_test unless Rails.env.production? 
-    @test_ambassador_tracking_id = "aaaaaaaaaa"
-    @test_mission_tracking_id = "bbbbbbbbbb"
-
-    ambassador = Ambassador.where(tracking_id: @test_ambassador_tracking_id).first
-    mission = Mission.where(tracking_id: @test_mission_tracking_id).first
-    if ambassador.nil? || mission.nil?
-      @points = "ningun amb or miss con amb_traker: #{@test_ambassador_tracking_id} or mis_traker: #{@test_mission_tracking_id}"
-    else
-      @points = ambassador.points.where(mission_id: mission.id).count
-    end
-  end
-
-  private
-  def seed_test
-    if Embassy.where(id: 1).empty?
-      embassy = Embassy.new(id: 1)
-      embassy.name ="Embassy for test"
-      embassy.save!
-    end
-    if Ambassador.where(id: 1).empty?
-      ambassador = Ambassador.new(id: 1)
-      ambassador.name = "The Ambassador"
-      ambassador.password = "foobar"
-      ambassador.email = "the_ambassador@example.com"
-      ambassador.tracking_id = "aaaaaaaaaa"
-      ambassador.embassy = embassy
-      ambassador.save!
-    end
-    if Mission.where(id: 1).empty?
-      mission = Mission.new(id: 1)
-      mission.name = "The Mission"
-      mission.short_description = "Short description for the Mission"
-      mission.tracking_id = "bbbbbbbbbb"
-      mission.tracking_url = "www.brandpage.com"
-      mission.embassy = embassy
-      mission.save!
-    end
-  end
 end
--- features/pixel.feature
+++
@@ -1,6 +0,0 @@
-Feature: as a Brand, I should have an easy way to track my Ambassador's achivements
-
-Scenario: Pixel tracking
-  Given I am a common web user
-  When I arrive at a page with a pixel tracker
-  Then I should increase the Points count for an Ambassador
