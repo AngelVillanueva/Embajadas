8b70c2bc23ad1549e5844253b7bb335929b47e70
Angel Villanueva <avillanueva@sinapse.es>
2013-01-31 13:47:04 +0100

    Previous require_belonging filter commented

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,6 @@
 class MissionsController < ApplicationController
   before_filter :authenticate_ambassador!, only: [:show]
-  before_filter :require_belonging, only: [:show]
+  #before_filter :require_belonging, only: [:show]
   
   def show
     @mission = Mission.find(params[:id])
