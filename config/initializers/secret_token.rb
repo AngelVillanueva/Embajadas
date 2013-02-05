88fea98e370c626606caefa24292b25af28e332a
Angel Villanueva <avillanueva@sinapse.es>
2013-02-05 16:59:50 +0100

    Change application name from Embajadas to Incitatus

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Embajadas::Application.config.secret_token = '729ce9266b8882f1d7726291c6aa8d37d8833e0a47abd4465dc6e21b982c60afd7d69aa597359fb3bf00cbc665bae966ce5204a8f4bb46e2fe89e0444ec20a02'
+Incitatus::Application.config.secret_token = '729ce9266b8882f1d7726291c6aa8d37d8833e0a47abd4465dc6e21b982c60afd7d69aa597359fb3bf00cbc665bae966ce5204a8f4bb46e2fe89e0444ec20a02'
