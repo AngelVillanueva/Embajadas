3a5b6841d387322dfd5b88865bd001155889623a
Angel Villanueva <avillanueva@sinapse.es>
2013-02-07 12:29:10 +0100

    Add custom validation to Assignment to ensure that Ambassador and Mission share the Embassy

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -19,8 +19,16 @@ class Assignment < ActiveRecord::Base
   before_validation :assign_code, :assign_tracking_url, :assign_short_url
 
   validates :ambassador_id, :mission_id, :code, :tracking_url, :short_url, presence: true
+  validate :same_scope?
 
   protected
+  # ambassador and mission belongs to the same embassy?
+  def same_scope?
+    if ambassador && mission
+      errors.add(:base, "Ambassador and Mission do not belong to the same Embassy") unless ambassador.embassies.include? mission.embassy
+    end
+  end
+  # automatically assigns a tracking_code for any new assignment (example: offline tracking?)
   def assign_code
     if code.nil?
       self.code = get_unique_tracking_code
@@ -34,19 +42,10 @@ class Assignment < ActiveRecord::Base
       get_unique_tracking_code
     end
   end
+  # automatically builds the tracking url, containing both the ambassador tracking_id and the destination url (entered by the Consul for the Mission)
   def assign_tracking_url
     if tracking_url.nil?
       self.tracking_url = build_tracking_url(mission_id, ambassador_id)
-    end
-  end
-  def assign_short_url
-    require 'bitly'
-    Bitly.use_api_version_3
-    bitly = Bitly.new('embassyland', 'R_4a3b8272b7634f605382c8e02e809378')
-    if short_url.nil?
-      tu = build_tracking_url(mission_id, ambassador_id)
-      su = bitly.shorten(tu)
-      self.short_url = su.short_url
     end
   end
   def build_tracking_url(mission_id, ambassador_id)
@@ -57,5 +56,16 @@ class Assignment < ActiveRecord::Base
     protocol = ''
     protocol = 'http://' unless mt.match /^http:\/\//
     tracking_url = own << protocol << mt
+  end
+  # automatically shortens the tracking url through bitly api
+  def assign_short_url
+    require 'bitly'
+    Bitly.use_api_version_3
+    bitly = Bitly.new('embassyland', 'R_4a3b8272b7634f605382c8e02e809378')
+    if short_url.nil?
+      tu = build_tracking_url(mission_id, ambassador_id)
+      su = bitly.shorten(tu)
+      self.short_url = su.short_url
+    end
   end
 end
