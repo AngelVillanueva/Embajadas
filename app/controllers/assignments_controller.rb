89b8245b42d4ea3a1e1dff09f9b40f577c9e0c4b
Angel Villanueva <avillanueva@sinapse.es>
2013-02-07 15:44:51 +0100

    Adjust CanCan abilities and feature tests to HABTM Ambassadors_Embassies

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,27 +1,28 @@
 class AssignmentsController < ApplicationController
   def create
     assignment = Assignment.new
-    mission_id = Mission.find_by_tracking_id(params[:assignment][:mission]).id
+    mission= Mission.find_by_tracking_id(params[:assignment][:mission])
 
-    assignment.mission_id = mission_id
+
+    assignment.mission_id = mission.id
     assignment.ambassador_id = current_ambassador.id
     if assignment.save
       flash[:success] = I18n.t("flash.Mission accepted")
     else
       flash[:error] = I18n.t("flash.Mission could not be accepted")
     end
-    redirect_to embassy_path(current_ambassador.embassy_id)
+    redirect_to embassy_path(mission.embassy_id)
   end
 
   def destroy
-    mission_id = Mission.find_by_tracking_id(params[:assignment][:mission]).id
-    assignment = Assignment.where(mission_id: mission_id, ambassador_id: current_ambassador.id).first
+    mission = Mission.find_by_tracking_id(params[:assignment][:mission])
+    assignment = Assignment.where(mission_id: mission.id, ambassador_id: current_ambassador.id).first
     
     if assignment.destroy
       flash[:success] = I18n.t("flash.Mission rejected")
     else
       flash[:error] = I18n.t("flash.Mission could not be rejected")
     end
-    redirect_to embassy_path(current_ambassador.embassy_id)
+    redirect_to embassy_path(mission.embassy_id)
   end
 end
