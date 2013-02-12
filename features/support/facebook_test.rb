a2f9b19f6048a5b290bcb08882d08033fcf4dec1
Angel Villanueva <avillanueva@sinapse.es>
2013-02-12 16:36:51 +0100

    Refactoring: support feature module to create a test user account via Facebook api

---
+++ features/support/facebook_test.rb
@@ -0,0 +1,11 @@
+module FacebookSteps
+  def create_Facebook(name, app_id, permissions, token)
+    require 'open-uri'
+    uri = "https://graph.facebook.com/#{app_id}/accounts/test-users?installed=true&name=#{name}&locale=en_US&permissions=#{permissions}&method=post&access_token=#{token}"
+    url = URI.parse(URI.encode(uri.strip))
+    response = open(url)
+    code = JSON.parse(response.string)
+  end
+end
+
+World(FacebookSteps)
