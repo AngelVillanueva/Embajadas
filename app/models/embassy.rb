f53875b479696743ad9dd235f58e196e5f425218
Angel Villanueva <avillanueva@sinapse.es>
2013-02-12 18:24:23 +0100

    Embassy instance method to filter the assigned Slogans
    assigned Slogans = Slogans belonging to one of its Missions

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -18,6 +18,10 @@ class Embassy < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
 
+  def assigned_slogans
+    Slogan.where(mission_id: mission_ids)
+  end
+
   def available_search_terms
     assigned_slogan_ids = Slogan.where(mission_id: self.mission_ids).map(&:search_term_id)
     SearchTerm.where(id: assigned_slogan_ids)
