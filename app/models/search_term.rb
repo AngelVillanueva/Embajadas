f36e238b95b40f3e875acb61ffcc2137b89219ca
Angel Villanueva <avillanueva@sinapse.es>
2013-02-13 16:54:11 +0100

    Model annotated : Ambassador, Mission, SearchTerm, Slogan

--- app/models/search_term.rb
+++ app/models/search_term.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: search_terms
+#
+#  id         :integer          not null, primary key
+#  term       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  consul_id  :integer
+#
+
 class SearchTerm < ActiveRecord::Base
   attr_accessible :term
   belongs_to :consul
--- app/models/slogan.rb
+++ app/models/slogan.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: slogans
+#
+#  id             :integer          not null, primary key
+#  mission_id     :integer
+#  search_term_id :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Slogan < ActiveRecord::Base
   belongs_to :mission
   belongs_to :search_term
