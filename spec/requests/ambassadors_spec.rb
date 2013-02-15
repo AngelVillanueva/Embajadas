62833b83db1f9c3bfe541ea0d7a844f554738bf0
Angel Villanueva <avillanueva@sinapse.es>
2013-02-15 17:36:10 +0100

    Refactorin: Ambassador spec test, including new method 'assigned_slogans'

--- spec/requests/ambassadors_spec.rb
+++ spec/requests/ambassadors_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe "Ambassadors" do
+  let(:ambassador_f) { FactoryGirl.create(:ambassador) }
+  let(:embassy_f) { FactoryGirl.create(:embassy) }
+  let(:consul_f) { FactoryGirl.create(:consul, embassy: embassy_f) }
+  let(:mission_f) { FactoryGirl.create(:mission, embassy: embassy_f) }
+  let(:search_term_f) { FactoryGirl.create(:search_term, term: "My loved brand", consul: consul_f) }
   describe "Model" do
-    let(:ambassador) { FactoryGirl.create(:ambassador) }
-    let(:embassy) { FactoryGirl.create(:embassy) }
-    before { ambassador.embassies << embassy }
-    subject { ambassador }
+    before { ambassador_f.embassies << embassy_f }
+    subject { ambassador_f }
 
     it { should respond_to :name}
     it { should respond_to :embassies }
@@ -81,27 +84,23 @@ describe "Ambassadors" do
     it { should be_valid }
   end
   describe "belonging to an Embassy" do
-    let(:embassy) { FactoryGirl.create(:embassy) }
-    let(:ambassador) { FactoryGirl.create(:ambassador) }
-    before { ambassador.embassies << embassy }
-    subject { ambassador }
+    before { ambassador_f.embassies << embassy_f }
+    subject { ambassador_f }
 
     it { should be_valid }
-    its(:embassies) { should include(embassy) }
+    its(:embassies) { should include(embassy_f) }
   end
   describe "with a mandatory tracking_id" do
-    let(:ambassador) { FactoryGirl.create(:ambassador)}
     before do
-      ambassador.tracking_id = ""
-      ambassador.save
+      ambassador_f.tracking_id = ""
+      ambassador_f.save
     end
-    subject { ambassador }
+    subject { ambassador_f }
 
     it { should_not be_valid }
   end
   describe "with a unique tracking_id" do
-    let(:ambassador) { FactoryGirl.create(:ambassador) }
-    let(:other_ambassador) { ambassador.dup }
+    let(:other_ambassador) { ambassador_f.dup }
     before do
       other_ambassador.email = "other@example.com"
     end
@@ -117,13 +116,13 @@ describe "Ambassadors" do
     its(:tracking_id) { should_not == nil }
   end
   describe "with slogans to use through assigned missions" do
-    let(:mission) { FactoryGirl.create(:mission) }
-    let(:slogan) { FactoryGirl.create(:slogan, mission: mission) }
-    let(:ambassador) { FactoryGirl.create(:ambassador) }
-    before { FactoryGirl.create(:assignment, ambassador: ambassador, mission: mission) }
+    before do
+      ambassador_f.embassies << embassy_f
+      FactoryGirl.create(:slogan, search_term: search_term_f, mission: mission_f)
+      FactoryGirl.create(:assignment, ambassador: ambassador_f, mission: mission_f)
+    end
+    subject { ambassador_f }
 
-    subject { ambassador.mission_ids }
-
-    its(:size) { should == 1 }
-  end
+    its(:assigned_slogans) { should have(1).item }
+    end
 end
