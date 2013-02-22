82b9812b8ab3f6ecdda611eafc089303ab41ca1f
Angel Villanueva <avillanueva@sinapse.es>
2013-02-22 10:16:36 +0100

    new Post model, annotated, spec test created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130213091156) do
+ActiveRecord::Schema.define(:version => 20130222091314) do
 
   create_table "ambassadors", :force => true do |t|
     t.string   "name"
@@ -101,6 +101,15 @@ ActiveRecord::Schema.define(:version => 20130213091156) do
     t.integer  "ambassador_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "message"
+    t.datetime "created_time"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "rails_admin_histories", :force => true do |t|
