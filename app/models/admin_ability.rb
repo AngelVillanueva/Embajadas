93d22d1fbf61d1100bb00c91011728998659edf5
Angel Villanueva <avillanueva@sinapse.es>
2013-03-15 21:33:21 +0100

    Consuls can manage its profile in admin area

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -12,6 +12,7 @@ class AdminAbility
       # Consuls can just manage its own resources
       else
         can :manage, Embassy, :id => consul.embassy_id
+        can :manage, Consul, :id => consul.id
         can :manage, Mission, :embassy_id => consul.embassy_id
         can :manage, Ambassador, ["id in (select ambassador_id from ambassadors_embassies where embassy_id=?)", consul.embassy_id] do |ambassador|
           ambassador.embassies.include? consul.embassy
