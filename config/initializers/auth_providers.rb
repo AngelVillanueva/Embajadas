8734acc70ddb419a711d7b0f25eb539d784c586c
Angel Villanueva <avillanueva@sinapse.es>
2013-04-12 00:08:57 +0200

    Move custom configs to ad-hoc folder (config/custom)

--- config/initializers/auth_providers.rb
+++ config/initializers/auth_providers.rb
@@ -1,7 +1,7 @@
 # Project custom configs
-PROJECT_CONFIG = YAML.load_file("#{::Rails.root}/config/the_project.yml")[::Rails.env]
+PROJECT_CONFIG = YAML.load_file("#{::Rails.root}/config/custom/the_project.yml")[::Rails.env]
 # OAuth providers
-TWITTER_CONFIG = YAML.load_file("#{::Rails.root}/config/twitter.yml")[::Rails.env]
-FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+TWITTER_CONFIG = YAML.load_file("#{::Rails.root}/config/custom/twitter.yml")[::Rails.env]
+FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/custom/facebook.yml")[::Rails.env]
 # External Apis
-BITLY_CONFIG = YAML.load_file("#{::Rails.root}/config/bitly.yml")[::Rails.env]
+BITLY_CONFIG = YAML.load_file("#{::Rails.root}/config/custom/bitly.yml")[::Rails.env]
