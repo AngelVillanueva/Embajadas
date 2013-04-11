7121477bc5b2f06cab61ef0029fd7de1de174044
Angel Villanueva <avillanueva@sinapse.es>
2013-04-11 13:30:11 +0200

    Complete MailingCode rspec test

--- app/models/mailing_code.rb
+++ app/models/mailing_code.rb
@@ -1,3 +1,37 @@
+# == Schema Information
+#
+# Table name: mailing_codes
+#
+#  id            :integer          not null, primary key
+#  tracking_code :string(255)
+#  embassy_id    :integer
+#  expires_at    :datetime
+#
+
 class MailingCode < ActiveRecord::Base
   belongs_to :embassy
+  attr_accessible :embassy_id, :expires_at
+
+  before_validation :assign_random_tracking_code
+
+  validates :tracking_code, :embassy_id, :expires_at, presence: true
+  validates :tracking_code, uniqueness: true
+
+  private
+  # assign a random tracking_code on MailingCode creation to avoid using externally app ids
+  def assign_random_tracking_code
+    if tracking_code.nil?
+      self.tracking_code = get_unique_tracking_code
+    end
+  end
+  # assign unique code
+  def get_unique_tracking_code
+    tracking_code = SecureRandom.hex 5
+    matches = MailingCode.where(tracking_code: tracking_code)
+    if matches.empty?
+      tracking_code
+    else
+      get_unique_tracking_code
+    end
+  end
 end
--- spec/models/mailing_codes_spec.rb
+++ spec/models/mailing_codes_spec.rb
@@ -2,10 +2,63 @@ require 'spec_helper'
 
 describe "MailingCodes" do
   let(:mailing_code) { MailingCode.new }
-  subject { mailing_code }
+  before do
+      mailing_code.tracking_code = "aaa"
+      mailing_code.embassy_id = "1"
+      mailing_code.expires_at = 1.week.from_now
+    end
+  describe "Model" do
+    subject { mailing_code }
 
-  it { should be_valid }
-  it { should respond_to :tracking_code }
-  it { should respond_to :embassy_id }
-  it { should respond_to :expires_at }
+    it { should be_valid }
+    it { should respond_to :tracking_code }
+    it { should respond_to :embassy_id }
+    it { should respond_to :expires_at }
+  end
+  describe "with all fields mandatory" do
+    before do
+      mailing_code.tracking_code = nil
+      mailing_code.embassy_id = nil
+      mailing_code.expires_at = nil
+      mailing_code.save
+    end
+    subject { mailing_code }
+
+    it { should_not be_valid }
+  end
+  describe "with auto-generated tracking code" do
+    before do
+      mailing_code.tracking_code = nil
+      mailing_code.save
+    end
+    subject { mailing_code }
+
+    its(:tracking_code) { should_not be_nil }
+    it { should be_valid }
+  end
+  describe "with unique tracking code" do
+    let(:mc) { MailingCode.new }
+    before do
+      mc.tracking_code = "aaa"
+      mc.embassy_id = "2"
+      mc.expires_at = 1.week.from_now
+      mailing_code.save
+      mc.save
+    end
+    subject { mc }
+
+    it { should_not be_valid }
+  end
+  describe "with accessible embassy_id field" do
+    let(:mc) { MailingCode.new(embassy_id: 1) }
+    subject { mc }
+
+    it { should_not be_valid }
+  end
+  describe "with accessible expires_at field" do
+    let(:mc) { MailingCode.new(expires_at: 1.day.from_now) }
+    subject { mc }
+
+    it { should_not be_valid }
+  end
 end
