6c4438083b8199fba252ffbb494c97f5909e9feb
Angel Villanueva <avillanueva@sinapse.es>
2013-04-24 13:12:31 +0200

    Correct assets path in uniform.css and select2.css

--- app/assets/javascripts/rails_admin/admin.js
+++ app/assets/javascripts/rails_admin/admin.js
@@ -13,7 +13,6 @@
 //= require rails_admin/excanvas.min
 //= require rails_admin/jquery.uniform
 //= require rails_admin/select2.min
-//= require rails_admin/select2.min
 //= require rails_admin/jquery.dataTables.min
 //= require rails_admin/unicorn.tables
 //= require rails_admin/jquery.flot.min
